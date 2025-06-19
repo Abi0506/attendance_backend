bb800c5acf76c657657cb4c6b970d1d70af602af
Abi0506 <abishek25052006@gmail.com>
2025-06-19 20:20:55 +0530

    Refactor attendance routes to remove commented-out code and add exemption management functionality

--- main.js
+++ main.js
@@ -25,28 +25,7 @@ const esslFunctionsRouter = require("./routes/essl_functions");
 app.use("/api", esslFunctionsRouter);
 app.use("/api", loginRouter);
 app.use("/api", attendanceRouter);
-// const bcrypt = require('bcryptjs');
 
-// // Simulate stored hash in the database
-// let storedHash = null;
-// bcrypt.hash('I0215', 10, (err, hash) => {
-//   if (err) {
-//     console.error('Error hashing password:', err);
-//     return;
-//   }
-//   storedHash = hash;
-//   const inputPassword = 'I0215';
-//   console.log('Input password:', storedHash);
-// bcrypt.compare(inputPassword, '$2b$10$vqI/V7zF7v1EM8ES8mP7Ne01TM/polzcC2JVIllMubXyVlepBfNzG', (err, isMatch) => {
-//   if (err) throw err;
-//   if (isMatch) {
-//     console.log('✅ Password is correct!');
-//   } else {
-//     console.log('❌ Incorrect password.');
-//   }
-// });
-// });
-// Login input
 
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
